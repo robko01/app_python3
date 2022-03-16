1fb6e7ab7cb19dd45e28473a9e0a940cc96018bc
Orlin Dimitrov <orlin369@gmail.com>
2022-03-16 23:33:20 +0200

    Add two type of timers

---
+++ App/utils/timer.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+"""
+
+Robko 01 - Python Controll Software
+
+Copyright (C) [2020] [Orlin Dimitrov]
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+import time
+
+#region File Attributes
+
+__author__ = "Orlin Dimitrov"
+"""Author of the file."""
+
+__copyright__ = "Copyright 2020, Orlin Dimitrov"
+"""Copyrighter"""
+
+__credits__ = []
+"""Credits"""
+
+__license__ = "GPLv3"
+"""License
+@see http://www.gnu.org/licenses/"""
+
+__version__ = "1.0.0"
+"""Version of the file."""
+
+__maintainer__ = "Orlin Dimitrov"
+"""Name of the maintainer."""
+
+__email__ = "robko01@8bitclub.com"
+"""E-mail of the author."""
+
+__class_name__ = "TaskGUI"
+"""Task name."""
+
+#endregion
+
+class Timer():
+
+#region Attributes
+
+    __t0 = 0
+
+    __t1 = 0
+
+    __delta = 0
+
+    __update_rate = 1
+
+    __enable = False
+
+    __cb = None
+
+#endregion
+
+#region Propertyes
+
+    @property
+    def update_rate(self):
+        
+        return self.__update_rate
+
+    @update_rate.setter
+    def update_rate(self, value):
+        
+        self.__update_rate = value
+
+
+#endregion
+
+#region Constructor
+
+    def __init__(self):
+
+        pass
+
+#endregion
+
+#region Public Methods
+
+    def start(self):
+
+        self.__enable = True
+
+    def stop(self):
+
+        self.__enable = False
+
+    def set_cb(self, cb):
+
+        self.__cb = cb
+
+    def update(self):
+
+        if not self.__enable:
+            return
+
+        self.__t1 = time.time()
+
+        self.__delta = self.__t1 - self.__t0
+
+        if self.__delta >= self.update_rate:
+
+            self.__t0 = self.__t1
+
+            if self.__cb != None:
+                self.__cb()
+
+#endregion
