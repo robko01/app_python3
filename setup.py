d03859d1b095070bcc5512b0ea7b224a86ae6470
Orlin Dimitrov <orlin369@gmail.com>
2023-05-21 23:42:14 +0300

    Fix path to doted

--- setup.py
+++ setup.py
@@ -98,6 +98,5 @@ setup(
         'Topic :: Software Development',
         'Topic :: Robot Programming'
     ],
-    package_data={'': ['robko01/tasks/task_ui_qt/ui/*.ui']},
-    include_package_data=True
+    package_data={'robko01.tasks.task_ui_qt.ui': ['*.ui']}
 )
